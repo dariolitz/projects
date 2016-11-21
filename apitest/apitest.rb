94cb248310d3e3d2c4065a18397c5603ef43395d
Dario Litz <d.litz@lifecodexx.com>
2016-11-21 16:48:28 +0100

    Delete apitest.rb

--- apitest/apitest.rb
+++
@@ -1,6 +0,0 @@
-require 'rspotify'
-
-RSpotify.authenticate("34f62c6d0da248f18b5a1b9674b61ec9","c2344d5fb5ca4026b5aaef443763f528")
-
-me = RSpotify::User.find('11134525102')
-me.playlists.each {|playlist| puts playlist.name }
